e3602f60470735a82b469ae03844d119da9cae2b
voste126 <steveaustine126@gmail.com>
2024-10-25 10:28:48 +0000

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
-
+'''A script that reads stdin line by line and computes metrics'''
 
 import sys
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
+# Initialize storage for status codes and total file size
+cache = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
 counter = 0
 
 try:
     for line in sys.stdin:
-        line_list = line.split(" ")
+        line_list = line.split()
         if len(line_list) > 4:
             code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
+            try:
+                size = int(line_list[-1])
+                total_size += size
+            except ValueError:
+                continue  # Ignore lines with invalid file sizes
+
+            if code in cache:
                 cache[code] += 1
-            total_size += size
             counter += 1
 
         if counter == 10:
-            counter = 0
             print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+            for key in sorted(cache.keys()):
+                if cache[key] != 0:
+                    print('{}: {}'.format(key, cache[key]))
+            counter = 0
 
-except Exception as err:
+except (KeyboardInterrupt, SystemExit):
     pass
-
 finally:
     print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))i
+    for key in sorted(cache.keys()):
+        if cache[key] != 0:
+            print('{}: {}'.format(key, cache[key]))
+
