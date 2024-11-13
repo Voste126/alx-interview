7a452cd7c9f69da6078f59dcb7e2d0d2959c4d68
voste126 <steveaustine126@gmail.com>
2024-11-13 16:05:22 +0000

    steve austine

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,46 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Usage: ./0-starwars_characters.js <movie_id>');
+  process.exit(1);
+}
+
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Error: ${response.statusCode}`);
+    return;
+  }
+
+  const filmData = JSON.parse(body);
+  const characters = filmData.characters;
+
+  const printCharacter = (index) => {
+    if (index >= characters.length) {
+      return;
+    }
+    
+    request(characters[index], (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+      printCharacter(index + 1);
+    });
+  };
+
+  printCharacter(0);
+});
+
